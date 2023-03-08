f6dbe5d51d3dd2dd2ba8a27158ced4f1fdac93dc
bemrekalkan <bemre.kalkan@gmail.com>
2023-03-09 00:38:29 +0300

    BlogCard component started (Image tag added)

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import BlogCard from "@/components/BlogCard";
 
 const interFont = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className={interFont.className}>
       <body className={styles.container}>
         <Header />
+        <BlogCard/>
         <main>{children}</main>
         <Footer />
       </body>
--- src/components/BlogCard/index.js
+++ src/components/BlogCard/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 
+import styles from "./styles.module.css"
+
 const BlogCard = () => {
     return (
         <Link href="/">
-            <div>
+            <div className={styles.imageWrapper}>aüuiauieaieua
                 <Image
-                    src="https://picsum.photos/200/300"
+                    src="https://picsum.photos/seed/picsum/200/300"
                     alt="Blog Card Image"
-                    width={96}
-                    height={96} />
+                    fill />
             </div>
         </Link>
     )
