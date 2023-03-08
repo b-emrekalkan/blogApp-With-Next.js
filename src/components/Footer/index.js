55bd8da4f916a6c15b84d6041335f847df3de50b
bemrekalkan <bemre.kalkan@gmail.com>
2023-03-08 23:11:13 +0300

    header and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import styles from "./styles.module.css"
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className={styles.footer}>
+        <h4>Built with Next.js</h4>
+    </footer>
   )
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
-
+import styles from "./styles.module.css"
 const Header = () => {
   return (
-    <header>
-      <Link href="/">
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
         <span>BLOG</span>
       </Link>
       <nav>
